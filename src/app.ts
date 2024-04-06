4cabf514329a19117c79d40171daa7a520d1bce7
TylerKingsley <tyler@kingsleyrobins.com>
2024-04-06 16:10:24 +0200

    Initial setup

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,9 @@ app.use('/api', api);
 
 console.log('Environment', process.env.NODE_ENV)
 if (process.env.NODE_ENV == 'production') {
-	app.use(express.static('Client/dist'));
+	app.use(express.static('client/dist'));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '..','Client', 'dist', 'index.html'));
+		res.sendFile(path.resolve(__dirname, '..','client', 'dist', 'index.html'));
 	});
 }
 
